b5530a83c74ebc612c39ed8803619149118194b4
Kantoh <leduongkhang@gmail.com>
2015-10-07 07:20:31 +0700

    'changed'

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ A Symfony project created on October 5, 2015, 10:51 am.
 http://www.ekino.com/mix-wordpress-symfony-together/
 
 http://tutorial.symblog.co.uk/
+
+https://www.youtube.com/watch?v=AvLk3i2CBgw&feature=youtu.be
--- web/wordpress/wp-config.php
+++ web/wordpress/wp-config.php
@@ -26,13 +26,13 @@ define('DB_NAME', 'mtf');
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'root');
+define('DB_PASSWORD', '');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
 
 /** Database Charset to use in creating database tables. */
-define('DB_CHARSET', 'utf8mb4');
+define('DB_CHARSET', 'utf8');
 
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
